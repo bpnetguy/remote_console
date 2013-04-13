eaa1d92c98b6f4bcb64030868ced64c5792c00de
Binh Phan <binh.phan@gmail.com>
2013-04-13 18:13:13 -0500

    add start.sh

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 require.config({
-  baseUrl: "js",
+  baseUrl: "./js",
 
   paths : {
     "backbone":"libs/backbone/backbone",
---
+++ src/node/start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export PORT=3001
+forever start remote_console.js
